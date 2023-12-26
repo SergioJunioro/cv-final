de84c69f09f6594c567378adc86bc8f7ec634c49
SergioJunioro <92206059+SergioJunioro@users.noreply.github.com>
2023-12-26 18:38:16 +0300

    Add files via upload

---
+++ js/feedback.js
@@ -0,0 +1,13 @@
+var btn = document.querySelector("button");
+var cont = document.querySelector(".popUp");
+
+btn.addEventListener("click", function () {
+  cont.id = "show";
+});
+
+cont.addEventListener("click", function (e) {
+  if (e.target == this) {
+    this.id = "hidden";
+  }
+  e.preventDefault;
+});
